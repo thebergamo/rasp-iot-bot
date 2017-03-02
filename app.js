6935f53e568339fafbd979d95bb93b4a10e315c9
Marcos Bérgamo <marcos@thedon.com.br>
2017-03-02 12:47:15 +0000

    Refactor methods to use async

--- app.js
+++ app.js
@@ -3,11 +3,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-var admin = require("firebase-admin");
-var network = require('network');
-var iplocation = require('iplocation')
+const admin = require("firebase-admin");
+const network = require('network');
+const iplocation = require('iplocation')
+const rpio = require('rpio');
 
 const app = express()
+const verify = process.env.VERIFY_VALUE || 'stub'
+const token = process.env.TOKEN_VALUE
+
+admin.initializeApp({
+    credential: admin.credential.cert(process.env.PATH_AUTH),
+    databaseURL: "https://fiap-iot-bot.firebaseio.com"
+});
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -19,62 +27,71 @@ app.get('/', function (req, res) {
     res.send('Hi, I am a bot!')
 })
 
-var verify = process.env.VERIFY_VALUE
-
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === verify) {
-        res.send(req.query['hub.challenge'])
+        return res.send(req.query['hub.challenge'])
     }
+
     res.send('Error, wrong token')
 })
 
-admin.initializeApp({
-    credential: admin.credential.cert(process.env.PATH_AUTH),
-    databaseURL: "https://fiap-iot-bot.firebaseio.com"
-});
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    console.log(messaging_events);
-       var rpio = require('rpio');
+  const messagingEvents = req.body.entry[0].messaging || []
 
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
+  console.log(messagingEvents);
 
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            if (text == "turnon") {
-                var unix = Math.round(+new Date() / 1000);
-
-                rpio.open(12, rpio.OUTPUT, rpio.LOW);
-                rpio.write(12, rpio.HIGH);
-
-                sendTextMessage(sender, "Turning on the light 💡💡")
-                sendToDB(sender, text, unix)
-            }
-            else if (text == "turnoff") {
-                var unix = Math.round(+new Date() / 1000);
-
-                rpio.write(12, rpio.LOW);
-                sendTextMessage(sender, "Turning off the lights 🔌")
-                sendToDB(sender, text, unix)
-
-            }
-            else {
-                var unix = Math.round(+new Date() / 1000);
-                sendToDB(sender, "invalid", unix)
-                sendTextMessage(sender, text.substring(0, 200) + " " + "... hmm, I am only prepared to turnon or turnoff!")
-            }
-        }
+  async.map(messagesingEvents, iterator, (err, result) => {
+    if (err) {
+      return res.sendStatus(400);
     }
-    res.sendStatus(200)
+
+    return res.sendStatus(200);
+  })
+
+  function iterator (event, callback) {
+    const sender = event.sender.id
+    const message = event.message && event.message.text
+    const text = '';
+    const databaseText = message;
+    const possibleStates = {
+      turnon: function () {
+        rpio.open(12, rpio.OUTPUT, rpio.LOW);
+        rpio.write(12, rpio.high);
+        text = "Turning on the light 💡💡"
+      },
+      turnoff: function () {
+        rpio.write(12, rpio.high);
+        text = "Turning off the lights 🔌"
+      }
+    }
+    const state = possibleStates[message];
+
+    if (!possibleStates[message]) {
+      text = `${message.substring(0, 200)} ... hmm, I am only prepared to turnon or turnoff!`
+      databaseText = 'invalid'
+    } else {
+      possibleStates[message]()
+    }
+
+    const tasks = {
+      message: async.apply(sendTextMessage, sender, text),
+      database: async.apply(sendToDB, sender, databaseText)
+    };
+
+    async.parallel(tasks, (err) => {
+      if (err) {
+        return callback(err);
+      }
+
+      return callback();
+    });
+  }
 })
 
-var token = process.env.TOKEN_VALUE
 
-function sendTextMessage(sender, text) {
-    let messageData = { text: text }
+function sendTextMessage(sender, text, callback) {
+    const messageData = { text: text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: token },
@@ -86,31 +103,41 @@ function sendTextMessage(sender, text) {
     }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
+          return callback(err);
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
+          return callback(response.body.error);
         }
     })
 }
 
-function sendToDB(sender, text, unix) {
-    var db = admin.database();
-    var ref = db.ref("server/message");
-    var msgRef = ref.child("msg");
-    return network.get_public_ip(function (err, ip) {
-            iplocation(ip, function (error, res) {
-                var req = {
-                    recipient: { id: sender },
-                    message: text,
-                    timestamp: unix,
-                    location: {
-                        latitude: res.latitude,
-                        longitude: res.longitude
-                    },
-                };
-                msgRef.push().set(req);
-                return req;
-            })
-        });
+function sendToDB(sender, text, callback) {
+    const db = admin.database();
+    const ref = db.ref("server/message");
+    const msgRef = ref.child("msg");
+
+    async.waterfall([
+      (cb) => network.get_public_ip(cb),
+      (ip, cb) => iplocation(ip, cb)
+    ], (err, res) => {
+      if (err) {
+        return callback(err);
+      }
+
+      const req = {
+        recipient: { id: sender },
+        message: text,
+        timestamp: Math.round(Date.now() / 1000),
+        location: {
+          latitude: res.latitude,
+          longitude: res.longitude
+        },
+      };
+
+      msgRef.push().set(req);
+
+      return callback()
+    });
 }
 
 app.listen(app.get('port'), function () {
